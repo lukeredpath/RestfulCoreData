7d0af044a78dbc8ccc35c4809102060c7b4c4bd1
Luke Redpath <luke@lukeredpath.co.uk>
2010-03-20 20:00:04 +0000

    Make sure projects in sample app have an account.

--- SampleRestApp/app/models/project.rb
+++ SampleRestApp/app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :name, :account
+  
+  before_validation :set_account
+  
+  private
+  
+  def set_account
+    self.account = "Example Account"
+  end
 end
